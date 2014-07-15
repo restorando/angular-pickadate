a523f557a4f71890e80eafcfd9f61e5af80fdc80
Miguel Peixe <miguel@cardume.art.br>
2014-07-15 11:29:17 -0300

    add default date feature

--- src/angular-pickadate.js
+++ src/angular-pickadate.js
@@ -46,6 +46,7 @@
         require: 'ngModel',
         scope: {
           date: '=ngModel',
+          defaultDate: '=',
           minDate: '=',
           maxDate: '=',
           disabledDates: '='
@@ -81,7 +82,7 @@
           var minDate       = scope.minDate && dateUtils.stringToDate(scope.minDate),
               maxDate       = scope.maxDate && dateUtils.stringToDate(scope.maxDate),
               disabledDates = scope.disabledDates || [],
-              currentDate   = new Date();
+              currentDate   = (scope.defaultDate && dateUtils.stringToDate(scope.defaultDate)) || new Date();
 
           scope.dayNames    = $locale.DATETIME_FORMATS['SHORTDAY'];
           scope.currentDate = currentDate;
