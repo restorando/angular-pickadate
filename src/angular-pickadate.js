7e27b2f12caf7b26472aa9ad4f7cee310d469799
Benoît Bourgeois <bierdok@gmail.com>
2014-12-05 12:14:27 +0100

    allow html for navigation

--- src/angular-pickadate.js
+++ src/angular-pickadate.js
@@ -80,7 +80,7 @@
       };
     }])
 
-    .directive('pickadate', ['$locale', 'pickadateUtils', 'pickadateI18n', 'dateFilter', function($locale, dateUtils, i18n, dateFilter) {
+    .directive('pickadate', ['$locale', '$sce', pickadateUtils', 'pickadateI18n', 'dateFilter', function($locale, $sce, dateUtils, i18n, dateFilter) {
       return {
         require: 'ngModel',
         scope: {
@@ -95,8 +95,8 @@
           '<div class="pickadate">' +
             '<div class="pickadate-header">' +
               '<div class="pickadate-controls">' +
-                '<a href="" class="pickadate-prev" ng-click="changeMonth(-1)" ng-show="allowPrevMonth">{{t("prev")}}</a>' +
-                '<a href="" class="pickadate-next" ng-click="changeMonth(1)" ng-show="allowNextMonth">{{t("next")}}</a>' +
+                '<a href="" class="pickadate-prev" ng-click="changeMonth(-1)" ng-show="allowPrevMonth">' + $sce.trustAsHtml(i18n.t('prev')) + '</a>' +
+                '<a href="" class="pickadate-next" ng-click="changeMonth(1)" ng-show="allowNextMonth">' + $sce.trustAsHtml(i18n.t('next')) + '</a>' +
               '</div>'+
               '<h3 class="pickadate-centered-heading">' +
                 '{{currentDate | date:"MMMM yyyy"}}' +
@@ -132,7 +132,6 @@
 
           scope.dayNames    = dateUtils.buildDayNames(weekStartsOn);
           scope.currentDate = currentDate;
-          scope.t           = i18n.t;
 
           scope.render = function(initialDate) {
             initialDate = new Date(initialDate.getFullYear(), initialDate.getMonth(), 1, 3);
